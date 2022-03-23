cb07f69342ecfdd9d28a1fcb4acc61d9972b6bc6
olacin <olacin@users.noreply.github.com>
2022-03-24 00:13:22 +0100

    🎉 Initial commit

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -1,10 +1,8 @@
-// structs3.rs
+// structs3.rjjjjjjjjjj
 // Structs contain data, but can also have logic. In this exercise we have
 // defined the Package struct and we want to test some logic attached to it.
 // Make the code compile and the tests pass!
 // If you have issues execute `rustlings hint structs3`
-
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -16,22 +14,22 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // panic statement goes here...
+            panic!("Negative weight !");
         } else {
-            Package {
+            return Package {
                 sender_country,
                 recipient_country,
                 weight_in_grams,
-            }
+            };
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
